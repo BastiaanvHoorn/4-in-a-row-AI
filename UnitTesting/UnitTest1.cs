fe16094d5192ca5b9974722bd5fffbfafbe2d845
bartvanderhoeven <bartvanderhoeven12@gmail.com>
2015-10-05 23:06:06 +0200

    Created a Field class. This class contains very basic methods to read and manipulate the field. Still, many things need to be implemented in the Game class to actually use the new Field class. Also some other functions have to be added to the Field class.

---
+++ UnitTesting/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _4_in_a_row_;
+
+namespace UnitTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1_getPlayer()
+        {
+            Field f = new Field(new byte[] { 0, 64, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+            player actual = f.getPlayer(1, 1);
+
+            player expected = player.Alice;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod1_setPlayer()
+        {
+            Field actual = new Field(new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+            actual.setPlayer(1, 1, player.Alice);
+
+            Field expected = new Field(new byte[] { 0, 64, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+            //Assert.AreEqual(expected.Storage[1], actual.Storage[1]);
+            CollectionAssert.AreEqual(expected.Storage, actual.Storage);
+        }
+
+        [TestMethod]
+        public void TestMethod2_setPlayer()
+        {
+            Field actual = new Field(new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+            actual.setPlayer(1, 1, player.Bob);
+
+            Field expected = new Field(new byte[] { 0, 128, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+            //Assert.AreEqual(expected.Storage[1], actual.Storage[1]);
+            CollectionAssert.AreEqual(expected.Storage, actual.Storage);
+        }
+
+        [TestMethod, ExpectedException(typeof(InvalidMoveException))]
+        public void TestMethod3_setPlayer_invalid()
+        {
+            Field actual = new Field(new byte[] { 0, 64, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+            actual.setPlayer(1, 1, player.Bob);
+        }
+    }
+}
---
+++ engine/host/Field.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_in_a_row_
+{
+    public class Field
+    {
+		private static byte[] Bitmask = new byte[] { 0x03, 0x0C, 0x30, 0xC0 }; //This array is used for bit operations. The items represent the following binary values: 00000011, 00001100, 00110000, 11000000.
+		internal byte[] Storage; //The actual array that stores the field.
+        
+		public Field(byte[] input)
+		{
+			Storage = input;
+		}
+
+        /// <summary>
+        /// Gets which player owns the cell at the specified coordinates
+        /// </summary>
+        /// <param name="x">X-coordinate (row)</param>
+        /// <param name="y">Y-coordinate (column)</param>
+        /// <returns>The player who owns the specified cell</returns>
+		public player getPlayer(int x, int y)
+		{
+			int cellIndex = 6 * x + y;              //The cell position represented as an index in a one-dimensional row instead of a coordinate representation (two-dimensional)
+			int byteNumber = cellIndex >> 2;		//cellIndex / 4     Each byte can store 4 cells (two bits per cell). With this operation we determine which byte contains the needed information.
+			int bitNumber = cellIndex & 3;			//cellIndex % 4     The position of the bit (and with that the position of the cell) within the selected byte.
+			int bits = Storage[byteNumber] & Bitmask[bitNumber];    //Gets the bits that represent the wanted cell.
+			return (player)(bits >> (2 * bitNumber));		//bits / 4^bitNumber    Returns the player by converting int ´bits´ to enum ´player´. This requires a bitshift like this: 00110000 -> 00000011 or: 11000000 -> 00000011 to make sure that we return a value of 0 (00), 1 (01) or 2 (10).
+		}
+
+        /// <summary>
+        /// Stores the given player at the given cell coordinates
+        /// </summary>
+        /// <param name="x">X-coordinate</param>
+        /// <param name="y">Y-coordinate</param>
+        /// <param name="player">The player that needs to be stored</param>
+		public void setPlayer(int x, int y, player player)
+		{
+            int cellIndex = 6 * x + y;
+            int byteNumber = cellIndex >> 2;
+            int bitNumber = cellIndex & 3;
+            int playerNumber = (int)player << (2 * bitNumber);
+
+            if ((Storage[byteNumber] & Bitmask[bitNumber]) == 0)
+            {
+                Storage[byteNumber] |= (byte)playerNumber;
+            }
+            else
+            {
+                throw new InvalidMoveException(string.Format("The cell at ({0}, {1}) is already taken", x, y));
+            }
+		}
+
+		//public void foo(int value, int bit)
+		//{
+		//	//Get
+		//	bool b = ((value & Bitmask[bit]) != 0);
+
+		//	//Turn on bit
+		//	value |= Bitmask[bit];
+
+		//	//Turn off bit
+		//	value |= ~Bitmask[bit];
+		//}
+	}
+}
