5cb97a7a2099daf9b4dfc8e4fee92cd7a9175ba8
XD-face-me <Bastiaan97.bvh@gmail.com>
2015-10-30 01:44:18 +0100

    Added some command-line options. Not implemented yet

--- Simulator/Simulator.cs
+++ Simulator/Simulator.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Diagnostics;
 using System.Linq;
-using System.Security.Policy;
-using System.Text;
-using System.Threading.Tasks;
 using Engine;
 using Botclient;
 
@@ -13,71 +8,167 @@ namespace Simulator
 {
     class Simulator
     {
+
         static Game game;
-        private static bool tie = false;
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="args"></param>
-        private static void Main(string[] args)
+        private static int width = 7;
+        private static int height = 6;
+        private static int max_games = 1;
+        private static int games_played;
+        private static int games_won_alice;
+        private static int games_won_bob;
+        private enum log_mode
         {
-            while (true)
-            {
-                game = new Game(7, 6);
-                Console.WriteLine($"Created new game of {game.get_field().Width} by {game.get_field().Height}");
-                List<IPlayer> players = new List<IPlayer>()
-                {
-                    new Bot(Engine.players.Alice),
-                    new Bot(Engine.players.Bob)
-                };
-
-                do
-                {
-                    do_turn(players.Find(player => player.player == game.next_players)); //Execute the turn the player who's turn it is
-                } while (!(check_for_win() || tie));
-                if (tie)
-                {
-                    Console.WriteLine("The game is a tie");
-                }
-                else if (game.has_won(Engine.players.Alice))
-                {
-                    Console.WriteLine("Alice won the game");
-                }
-                else
-                {
-                    Console.WriteLine("Bob won the game");
-                }
-                Console.ReadLine();
-            }
-
+            silent,         //Nothing is logged
+            only_errors,    //Only errors are logged
+            essential,      //Loggs all less common messages and errors (default)
+            verbose,        //Loggs a lot of things
+            debug           //Loggs everything
         }
+
+        static private log_mode mode = log_mode.essential;
         /// <summary>
         /// Tries to execute the given turn from the given player.
         /// If the given turn is not possible for whatever reason, the player is asked again.
+        /// Returns false if it fails to drop a stone;
         /// </summary>
         /// <param name="player"></param>
-        static void do_turn(IPlayer player)
+        static bool do_turn(IPlayer player)
         {
+            if (game.stones_count == game.width * game.height) //If the whole field is full of stones and no one has won, it's a tie
+            {
+                return false;
+            }
             string s = "";
             int counter = 0;
-            while (!game.add_stone(player.get_turn(game.get_field()), player.player, ref s))
+            while (!game.add_stone(player.get_turn(game.get_field()), player.player, ref s)) //Try to add a stone fails. If that fails, log the error and try it again.
             {
-                if (s != "")
+                counter++;
+                Console.WriteLine($"{s} ({counter} tries)");
+                if (counter >= 100) //If we tried enough times to add a stone, exit the game to prevent infinite looping
                 {
-                    counter++;
-                    Console.WriteLine($"{s} ({counter} tries)");
-                    if (counter > 100) //Lazy way to prevent the simulator from locking itself if it can't add any new stones
-                    {
-                        tie = true;
-                        break;
-                    }
+                    Console.WriteLine("Exceeded maximum of tries for a turn");
+                    return false;
                 }
             }
+            return true;
         }
-        static bool check_for_win()
+
+        static void do_game()
         {
-            return game.has_won(players.Alice) || game.has_won(players.Bob);
+            game = new Game((byte)width, (byte)height);
+            Console.WriteLine($"Created new game of {game.get_field().Width} by {game.get_field().Height}");
+            List<IPlayer> _players = new List<IPlayer>() //A fancy list to prevent the use of if-statements
+                {
+                    new Bot(players.Alice),
+                    new Bot(players.Bob)
+                };
+            bool tie = false;
+            do
+            {
+                tie = !do_turn(_players.Find(player => player.player == game.next_players)); //Execute the turn the player who's turn it is. If do_turn returns false, it is a tie;
+            } while (!(game.has_won(players.Alice) || game.has_won(players.Bob) || !tie)); //Keep processing turns if no one has won and it isn't a tie
+            if (tie)
+            {
+                Console.WriteLine("The game is a tie");
+            }
+            else if (game.has_won(players.Alice))
+            {
+                Console.WriteLine("Alice won the game");
+            }
+            else
+            {
+                Console.WriteLine("Bob won the game");
+            }
+            Console.ReadLine();
+        }
+
+        private static void init(string[] _args)
+        {
+            List<string> args = new List<string>(_args);
+            int log_mode_index = (args.IndexOf("-l"));
+            if (log_mode_index != -1)
+            {
+                try
+                {
+                    byte log_mode_arg = byte.Parse(args[log_mode_index + 1]);
+                    if (log_mode_arg < 5)
+                    {
+                        mode = (log_mode) log_mode_arg;
+                        Console.WriteLine($"Log mode set to {mode}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{log_mode_arg} is no valid log mode.");
+                        Console.WriteLine($"Log mode defaulted to {mode} (essential)");
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("log mode wasn't given in the right format.");
+                    Console.WriteLine($"Log mode defaulted to {mode}");
+                }
+            }
+
+            int width_index = (args.IndexOf("-w"));
+            if (width_index != -1)
+            {
+                try
+                {
+                    width = byte.Parse(args[width_index + 1]);
+                    Console.WriteLine($"Set width to {width}");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Width wasn't given in the right format.");
+                    Console.WriteLine($"Width defaulted to {width}");
+                }
+            }
+
+            int height_index = (args.IndexOf("-h"));
+            if (height_index != -1)
+            {
+                try
+                {
+                    height = byte.Parse(args[height_index + 1]);
+                    Console.WriteLine($"Set height to {height}");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Height wasn't given in the right format.");
+                    Console.WriteLine($"Height defaulted to {height}");
+                }
+            }
+
+            int max_games_index = (args.IndexOf("-g"));
+            if (max_games_index != -1)
+            {
+                try
+                {
+                    max_games = byte.Parse(args[max_games_index + 1]);
+                    Console.WriteLine($"Set amount of games to {max_games}");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Amount of games wasn't given in the right format.");
+                    Console.WriteLine($"Amount of games defaulted to {max_games}");
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+
+        /// <param name="args">
+        /// -l  [0-5]   log mode (default 2) <see cref="log_mode"/>
+        /// -w  [>0]    width  of the playing field (default 7)
+        /// -h  [>0]    height of the playing field (default 6)
+        /// -g  [>0]    amount of games to simulate (default 1)
+        /// </param>
+        private static void Main(string[] args)
+        {
+            init(args);
+            do_game();
+
         }
     }
 }
